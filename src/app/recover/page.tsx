eeeef039afc4308f2d6af2a77290c4f8826a958a
joa7b <joa7@protonmail.com>
2025-07-14 00:03:40 -0300

    build: recover pass build issues

--- src/app/recover/page.tsx
+++ src/app/recover/page.tsx
@@ -1,15 +1,18 @@
 import { Recover } from "@/components/Recover";
+import { Suspense } from "react";
 
 export default function RecoverPage() {
   return (
-    <main>
-      <Recover
-        logo={{
-          url: "https://vertify.com.br",
-          alt: "Vertify",
-          src: "https://vertify-public-assets.s3.us-east-2.amazonaws.com/logos/1svg.svg",
-        }}
-      />
-    </main>
+    <Suspense fallback={<div>Carregando...</div>}>
+      <main>
+        <Recover
+          logo={{
+            url: "https://vertify.com.br",
+            alt: "Vertify",
+            src: "https://vertify-public-assets.s3.us-east-2.amazonaws.com/logos/1svg.svg",
+          }}
+        />
+      </main>
+    </Suspense>
   );
 }
